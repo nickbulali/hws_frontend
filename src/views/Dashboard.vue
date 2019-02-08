b979d3c6f5b28c7cb62ce27a03261bc8258976af
13enjamin <benjaminodonya@gmail.com>
2019-02-08 20:51:08 +0300

    Routes

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="dashboard">
+  	<h1 class="subheading grey--text">Dashboard</h1>
+
+  	<v-container class="my-5">
+  		
+
+  			<v-layout row wrap>
+  				<v-flex xs6 sm4 md3>
+  					<v-card app class="pa-3 ma-1">
+  						<div class="caption grey--text">User Card</div>
+  					</v-card>
+  				</v-flex>
+  				<v-flex xs12 md6>
+  					<v-card class="pa-3 ma-1">
+  						<div class="caption grey--text">Jobs</div>
+  					</v-card>
+  				</v-flex>
+  				<v-flex xs12 md3>
+  					<v-card class="pa-3 ma-1">
+  						<div class="caption grey--text">Top Jobs</div>
+  					</v-card>
+  					<v-card class="pa-3 ma-1">
+  						<div class="caption grey--text">Most Viewed Companies</div>
+  					</v-card>
+  				</v-flex>
+  			</v-layout>
+  		
+  	</v-container>
+
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+  }
+</script>
